b2d37d275389acf35c8d73d49e2f08f2b8fd8ef9
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2018-03-01 15:17:38 +0700

    Add two factor authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
+  devise :two_factor_authenticatable,
+   otp_secret_encryption_key: "9a6a82ea0f1b9b3cf080938e830315c9ab934c740bcbdcd17af9dac22dfe68c9f233a8c729714db07089ca0974ce0f6f8b0787259589c7175d2b6a3ff93e11f2"
+  devise :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 end

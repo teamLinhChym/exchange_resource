b067006f85a11307fe291e55319a13d1e1e552b3
huyliver6793 <huynd6793@gmail.com>
2018-03-14 17:24:25 +0700

    add_admin_blur

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+
+  namespace :manager do
+    root to: "static_page#dashboard"
+  end
 end
---
+++ exchange_admin/src/app/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('BlurAdmin', [
+  'ngAnimate',
+  'ui.bootstrap',
+  'commonLib'
+]).config(['$httpProvider', '$locationProvider', defaultConfig]);
+
+/*
+ * Default config for $http
+ * Data Type: json
+ * Always send csrf token
+*/
+function defaultConfig($httpProvider, $locationProvider) {
+  var csrfTokenElm = document.getElementsByName('csrf-token'),
+      csrfToken = csrfTokenElm[0] ? csrfTokenElm[0].content : '';
+  $httpProvider.defaults.headers.common.Accept = 'application/json';
+  $httpProvider.defaults.headers.post['X-CSRF-Token'] = csrfToken;
+  $httpProvider.defaults.headers.put['X-CSRF-Token'] = csrfToken;
+  $httpProvider.defaults.headers.delete = {'X-CSRF-Token': csrfToken};
+}

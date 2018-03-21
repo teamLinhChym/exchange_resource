3d0ea4ca33453e9016622184a8ebd1a370e19689
doan.chi.nhat <FRAMGIA\doan.chi.nhat@b120725-pc.framgia.com>
2018-03-21 17:54:01 +0700

    Create Skill table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,18 @@
-User.create! email: "test@gmail.com", password: 123123123
+puts "Creating Users ..."
+User.destroy_all
+20.times do
+  user_params = {
+    email: Faker::Internet.unique.email,
+    password: 123123123
+  }
+  User.create(user_params)
+end
+
+puts "Creating Skills ..."
+Skill.destroy_all
+20.times do
+  skill_params = {
+    name: Faker::Job.unique.field
+  }
+  Skill.create(skill_params)
+end

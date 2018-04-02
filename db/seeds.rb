da9137aba80912de2f132bfed4e6fab4b9066deb
nguyen.h.a.dung <FRAMGIA\nguyen.h.a.dung@b120645-lt.framgia.com>
2018-04-02 10:23:17 +0700

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,25 @@
+puts "Create Companies ..."
+
+Company.destroy_all
+10.times do
+  name = Faker::Company.name
+  email = "#{name}@gmail.com"
+  company_params = { name: name, email: email }
+
+  Company.create! company_params
+end
+
 puts "Creating Users ..."
-User.destroy_all
-20.times do
-  user_params = {
-    email: Faker::Internet.unique.email,
-    password: 123123123
-  }
-  User.create(user_params)
+
+Company.all.each do |company|
+  3.times do
+    user_params = {
+      email: Faker::Internet.unique.email,
+      password: 123123123,
+      company_id: company.id
+    }
+    User.create! user_params
+  end
 end
 
 puts "Creating Skills ..."
@@ -14,5 +28,5 @@ Skill.destroy_all
   skill_params = {
     name: Faker::Job.unique.field
   }
-  Skill.create(skill_params)
+  Skill.create! skill_params
 end
